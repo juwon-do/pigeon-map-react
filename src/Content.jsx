4e9ab7b673a06840f9a8d1c68cd633c71e35a332
Juwon Do <nobeldr13@gmail.com>
2023-01-24 12:53:15 -0600

    add pigeon map

---
+++ src/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Map, Marker } from "pigeon-maps"
+import { MyMap } from "./MyMap"
+export function Content() {
+  return (
+    <div>
+      <h1>Welcome to React!</h1>
+
+      <MyMap />
+    </div>
+  )
+}
