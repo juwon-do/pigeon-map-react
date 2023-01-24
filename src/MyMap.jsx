b39425e3e0938ee5a3ba40158e742a416cc0348b
Juwon Do <nobeldr13@gmail.com>
2023-01-24 13:13:08 -0600

    add marker

--- src/MyMap.jsx
+++ src/MyMap.jsx
@@ -1,18 +1,32 @@
 import React, { useState } from 'react'
-import { Map } from 'pigeon-maps'
+import { Map, Marker, Overlay } from 'pigeon-maps'
+import { stamenTerrain } from 'pigeon-maps/providers'
 
 export function MyMap() {
-  const [center, setCenter] = useState([50.879, 4.6997])
+  const [center, setCenter] = useState([41.866333, -87.606783])
   const [zoom, setZoom] = useState(11)
+  const [hue, setHue] = useState(0)
+  const color = `hsl(${hue % 360}deg 39% 70%)`
   return (
     <Map 
-      height={300}
+      height={500}
       center={center} 
       zoom={zoom} 
+ 
+      
       onBoundsChanged={({ center, zoom }) => { 
         setCenter(center) 
         setZoom(zoom) 
       }} 
-    />
+      
+    >
+      <Marker 
+        width={50}
+        anchor={[41.866333, -87.606783]} 
+        color={color} 
+        onClick={() => setHue(hue + 20)} 
+      />
+    </Map>
+    
   )
 }
